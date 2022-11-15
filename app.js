3b0ee0d4dd3e51ddda64d432c2a424b9a614f966
gmi Rushi <117174825+gmrushi@users.noreply.github.com>
2022-11-15 16:14:17 +0530

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req ,res) => {
+ res.send( '<h1>Rushi rushi Express App<h1> <h4>message: success<h4>') 
+})
+
+app.listen(port, ()=> {
+    console.log(`listning to port: ${port}`);
+    
+})
