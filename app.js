d8d5e2de4579bdd5b75b65c7cf00bfd3a1b86c8a
gmi Rushi <117174825+gmrushi@users.noreply.github.com>
2022-11-16 17:15:59 +0530

    update app

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req ,res) => {
-<<<<<<< HEAD
  res.send( '<h1>Rushi dev6 Express App<h1> <h4>message: success<h4>') 
-=======
- res.send( '<h1>Rushi dev5 Express App<h1> <h4>message: success<h4>') 
->>>>>>> a9b5e1bd64abc48ffb653dc5d895cb14b423cabe
 })
 
 app.listen(port, ()=> {
